e731fbe21a4cff70d47b1557d771743ff36a9a0d
Rian Botha <rianbotha@Rian-Bothas-MacBook-Pro.local>
2018-02-10 07:56:47 +0200

    Updated to fit our structure

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 #!/usr/bin/env node
-const reactDocs = require('react-docgen')
-const fs = require('fs')
-const path = require('path')
+const reactDocs = require('react-docgen');
+const fs = require('fs');
+const path = require('path');
 
-const filename = process.argv[process.argv.length - 1]
-const content = fs.readFileSync(path.resolve(process.cwd(), filename), 'utf-8')
+const filename = process.argv[process.argv.length - 1];
+const content = fs.readFileSync(path.resolve(process.cwd(), filename), 'utf-8');
 
-const component = reactDocs.parse(content)
-console.log('|Name|Type|Default|Description|')
-console.log('|---|---|---|---|')
+const component = reactDocs.parse(content);
+console.log('| Property | PropType | Required | Default | Description |');
+console.log('|----------|----------|----------|---------|-------------|');
 for (const name of Object.keys(component.props)) {
-  const { type, required, description, defaultValue } = component.props[name]
-  console.log(`|${name}${required ? '*' : ''}|\`${formatType(type)}\`|${defaultValue != null ? `\`${defaultValue.value}\`` : ''}|${description}|`)
-}
-if (Object.keys(component.props).some((name) => component.props[name].required)) {
-  console.log('')
-  console.log('\\* required property')
+  const { type, required, description, defaultValue } = component.props[name];
+  console.log(`| ${name}$ | \`${formatType(type)}\` | ${required ? 'yes' : '-'} | ${defaultValue != null ? `\`${defaultValue.value}\`` : ''} | ${description} |`);
 }
 
 function formatType (type) {
   if (type.name === 'union') {
-    return type.value.map(formatType).join('|')
+    return type.value.map(formatType).join('|');
   } else {
-    return type.name
+    return type.name;
   }
 }
