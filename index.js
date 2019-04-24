27ed3faddf2f3c489c9e3fd22591a3e46a8a43ca
rianbotha <hello@rian.co.za>
2019-04-24 07:38:10 +0200

    Handle line breaks in props

--- index.js
+++ index.js
@@ -9,15 +9,20 @@ const content = fs.readFileSync(path.resolve(process.cwd(), filename), 'utf-8');
 const component = reactDocs.parse(content);
 console.log('| Property | PropType | Required | Default | Description |');
 console.log('|----------|----------|----------|---------|-------------|');
+
 for (const name of Object.keys(component.props)) {
   const { type, required, description, defaultValue } = component.props[name];
-  console.log(`| ${name} | \`${formatType(type)}\` | ${required ? 'yes' : ''} | ${defaultValue != null ? `\`${defaultValue.value}\`` : ''} | ${description} |`);
+  console.log(`| ${name} | \`${formatType(type)}\` | ${required ? 'yes' : ''} | ${defaultValue != null ? `\`${nlToBr(defaultValue.value, true)}\`` : ''} | ${nlToBr(description)} |`);
 }
 
 function formatType (type) {
   if (type.name === 'union') {
-    return type.value.map(formatType).join('|');
+    return type.value.map(formatType).join(' || ');
   } else {
     return type.name;
   }
+}
+
+function nlToBr(str, addQuotes = false) {
+  return str.replace(/\r\n|\r|\n/gi, addQuotes ? '`<br>`' : '<br>');
 }
